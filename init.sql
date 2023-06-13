d38d91bc2e33d97d964a87b0b87492aa9fabea9c
Lucas Carranza <lucas.carranza@utec.edu.pe>
2023-06-13 16:37:19 -0500

    3fn

--- init.sql
+++ init.sql
@@ -6,6 +6,10 @@ CREATE SCHEMA grupo10;
 
 SET search_path TO grupo10;
 
+-- 3fn
+-- dni -> n,a,s,ruc
+-- ruc -> n,a,s,dni
+-- dni y ruc son llaves candidatas
 CREATE TABLE mecanico (
     dni varchar(8) PRIMARY KEY,
     nombres varchar(50) NOT NULL,
@@ -14,6 +18,10 @@ CREATE TABLE mecanico (
     salario numeric(10,2) NOT NULL
 );
 
+-- 3fn
+-- dni -> n,a,s
+-- ruc -> n,a,s
+-- dni y ruc son llaves candidatas
 CREATE TABLE asesor (
     dni varchar(8) PRIMARY KEY,
     nombres varchar(50) NOT NULL,
@@ -22,17 +30,23 @@ CREATE TABLE asesor (
     salario numeric(10,2) NOT NULL
 );
 
+-- 3fn
+-- dni -> n,a
 CREATE TABLE cliente (
     dni varchar(8) PRIMARY KEY,
     nombres varchar(50) NOT NULL,
     apellidos varchar(50) NOT NULL
 );
 
+-- 3fn
+--  ruc -> rs
 CREATE TABLE empresa (
-    RUC varchar(11) PRIMARY KEY,
+    ruc varchar(11) PRIMARY KEY,
     razonsocial varchar(50) NOT NULL UNIQUE
 );
 
+-- 3fn
+-- dni -> n,a,s,ruc
 CREATE TABLE representante (
     dni varchar(8) PRIMARY KEY,
     nombres varchar(50) NOT NULL,
@@ -40,6 +54,10 @@ CREATE TABLE representante (
     empresa_RUC varchar(11) NOT NULL REFERENCES empresa(RUC)
 );
 
+-- 3fn
+-- id -> m,n,a,p,c
+-- m,n,a -> id,p,c
+-- m,n,a e id son llaves candidatas
 CREATE TABLE modelo (
     id int PRIMARY KEY,
     marca varchar(20) NOT NULL,
@@ -49,6 +67,7 @@ CREATE TABLE modelo (
     categoria varchar(10) NOT NULL
 );
 
+-- 3fn
 CREATE TABLE motor (
     codigo varchar(20) PRIMARY KEY,
     marca varchar(20) NOT NULL,
@@ -58,6 +77,7 @@ CREATE TABLE motor (
     potencia int NOT NULL
 );
 
+-- 3fn 
 CREATE TABLE vehiculo (
     vin varchar(17) PRIMARY KEY,
     color varchar(20) NOT NULL,
@@ -70,6 +90,10 @@ CREATE TABLE vehiculo (
     suministro_codigo int NOT NULL REFERENCES suministro(codigo)
 );
 
+-- 3fn
+-- codigo -> f,cdni,vvin,adni
+-- vehiculo_vin -> f,cdni,vvin,adni
+-- vehiculo_vin y codigo son llaves candidatas
 CREATE TABLE compra (
     codigo int PRIMARY KEY,
     fecha date NOT NULL,
@@ -78,12 +102,16 @@ CREATE TABLE compra (
     asesor_dni varchar(8) NOT NULL REFERENCES asesor(dni)
 );
 
+-- 3fn
+-- codigo -> f, rdni
 CREATE TABLE suministro (
     codigo int PRIMARY KEY,
     fecha date NOT NULL,
     representante_dni varchar(8) NOT NULL REFERENCES representante(dni)
 );
 
+-- 3fn
+-- ccodigo, orden -> fi,ff,c,t
 CREATE TABLE serviciopostventa (
     compra_codigo int NOT NULL REFERENCES compra(codigo),
     orden int NOT NULL,
@@ -94,18 +122,23 @@ CREATE TABLE serviciopostventa (
     PRIMARY KEY (compra_codigo, orden)
 );
 
+-- 3fn
+-- nro -> f,vvin
 CREATE TABLE inspeccion (
     nro int PRIMARY KEY,
     fecha date NOT NULL,
     vehiculo_vin varchar(17) NOT NULL REFERENCES vehiculo(vin)
 );
 
+-- 3fn
 CREATE TABLE m_inspecciona (
     mecanico_dni varchar(8) NOT NULL REFERENCES mecanico(dni),
     inspeccion_nro int NOT NULL REFERENCES inspeccion(nro),
     PRIMARY KEY (mecanico_dni, inspeccion_nro)
 );
 
+-- 3fn
+-- mdni, ccodigo, sorden -> f, p
 CREATE TABLE m_trabaja (
     mecanico_dni varchar(8) NOT NULL REFERENCES mecanico(dni),
     compra_codigo int NOT NULL,
